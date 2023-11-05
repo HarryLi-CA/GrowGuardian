428a3ad8d0f0188267437c640796e7951a568c83
HarryTheCoder-L <69062529+HarryTheCoder-L@users.noreply.github.com>
2023-11-05 07:39:25 -0500

    Hosting and Auth0 Setup

--- src/Wrapper.js
+++ src/Wrapper.js
@@ -1,9 +1,26 @@
 import App from './App';
+import { withAuthenticationRequired } from '@auth0/auth0-react';
+import {user, isAuthenticated, isLoading, useAuth0 } from "@auth0/auth0-react";
 //import Auth from './Auth';
 
+const PrivateRoute = () => <App />;
 function Wrapper() {
-  return (
-    <App/>
-  );
+    const { user, isAuthenticated, isLoading } = useAuth0();
+    const {loginWithRedirect} = useAuth0();
+    if (isLoading) {
+        //return <div>Loading ...</div>;
+        console.log("Loading");
+      }
+    if(isAuthenticated){
+        console.log("Loaded");
+       /* return (
+            
+              <div>
+                Loaded
+              </div>
+            );*/
+    }
+    return <button onClick={() => loginWithRedirect()}>Log In</button>;
+
 }
 export default Wrapper;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Auth0Provider } from '@auth0/auth0-react';
 import './index.css';
 import Wrapper from './Wrapper';
 import reportWebVitals from './reportWebVitals';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Auth0Provider
+    domain="dev-sa0me5m2m3g3n8i8.us.auth0.com"
+    clientId="9lRaoay9EGYzUPbLyrZv7OKLQlB4L96O"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
     <Wrapper />
-  </React.StrictMode>
+    </Auth0Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
